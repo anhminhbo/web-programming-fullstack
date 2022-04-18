cd92c15d026901bef78caea673193c57535a607b
Minh Nguyens <minhnguyen@wee.vn>
2022-04-18 08:55:33 +0700

    update init

---
+++ www/index.php
@@ -0,0 +1,38 @@
+<!-- Homepage -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Homepage</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <!-- Logo -->
+        <img src="" alt="" />
+
+        <!-- Website name -->
+        <h1>Instakilogram</h1>
+
+        <!-- Search field -->
+        <section class="search-bar-container"></section>
+
+        <!-- Login or My account -->
+        <section class="header-login-container"></section>
+      </header>
+
+      <main></main>
+
+      <footer>
+        <ul class="footer-list">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ www/shareImages/shareImages.php
@@ -0,0 +1,5 @@
+<?php
+   if ($_POST['image'] && $_POST['shareOptions'] != 'select') {
+        
+   }
+?>
