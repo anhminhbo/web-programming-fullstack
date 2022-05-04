6e1c19ea70ed32e5e4fc6d8a807f2ba945d33513
Khuong <s3924577@rmit.edu.vn>
2022-05-04 17:00:26 +0700

    add "readDb" function and adjust relevant files

--- www/register/register-ss.php
+++ www/register/register-ss.php
@@ -1,8 +1,11 @@
 <?php
     session_start();
+
+    // Import utilities
     require_once('../util/imageHandler.php');
     require_once('../util/db.php');
-    //Initializing variables
+
+    // Initializing variables
 
     $fname = "";
     $lname = "";
@@ -12,14 +15,8 @@
     $target_dir = "../profileImgRepo/";
     $target_file = "";
     $imageFileType = "";
-    
-    //Connect to database
 
-    $read_db = file("../../accounts.db");
-    $write_db = fopen("../../accounts.db", "a");
-    $writeImg_db = fopen($target_dir . "profilePicture.db", "a");
-
-    //Register user
+    // Register user
 
     if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) && isset($_POST["pass"])) {
         
@@ -31,8 +28,9 @@
         $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
 
         // Check if fname in database
+        $read_db = readInfo("../../accounts.db", "r");
         foreach ($read_db as $line) {
-            $email_temp = strchr($line, "|", true);
+            $email_temp = $line[0];
             if ($email == $email_temp) {
                 array_push($errors, "Email duplicated.");
                 break;
@@ -42,12 +40,12 @@
         // Check the condition of profile picture
         if (isset($_FILES["profileImg"]) && $_FILES["profileImg"]["name"] != "") {
             $target_file = $target_dir . basename($_FILES["profileImg"]["name"]);
-            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
             if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
                 array_push($errors, "File type is wrong. Please upload a picture.");
             }
             if (file_exists($target_file)) {
-                array_push($errors,"Sorry, file already exists.");
+                array_push($errors, "Sorry, file already exists.");
             }
         }
         else {
@@ -58,33 +56,32 @@
         if (count($errors) == 0) {
             // Get created date
             date_default_timezone_set("Asia/Ho_Chi_Minh");
-            $createdDate = date("H:i:s d-m-Y",time());
+            $createdDate = date("H:i:s d-m-Y", time());
         
             // Combine data into a line
-            $result = [$email,$hashed_pass,$fname,$lname,$createdDate];
+            $result = [$email, $hashed_pass, $fname, $lname, $createdDate];
             // Store profile picture
             if ($target_file != $target_dir . "default.png") {
                 $new_img_name =  renameImg($imageFileType, $target_dir);
-                $uploadPath = $target_dir .$new_img_name;
+                $uploadPath = $target_dir . $new_img_name;
 
                 move_uploaded_file($_FILES["profileImg"]["tmp_name"], $uploadPath);
 
                 // Link profile image to an email
-                $img_result = [$email,$new_img_name];
+                $img_result = [$email, $new_img_name];
             }
             else {
                 // Link profile image to an email
-                $img_result = [$email,"default.png"];
+                $img_result = [$email, "default.png"];
             }
             
             // Store img info
             $profileDbRepoPath = "../profileImgRepo/profilePicture.db";
-            storeInfo($profileDbRepoPath,$img_result,"a");
+            storeInfo($profileDbRepoPath, $img_result, "a");
 
-            //store user info
-
+            // Store user info
             $accDbRepoPath =  "../../accounts.db";
-            storeInfo($accDbRepoPath,$result,"a");
+            storeInfo($accDbRepoPath, $result, "a");
     
             
             // Create SESSION
