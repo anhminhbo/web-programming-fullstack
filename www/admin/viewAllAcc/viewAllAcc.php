41d0fb6762bcb0af1881e5077a05bb8e99d3becf
Minh Nguyens <minhnguyen@wee.vn>
2022-05-16 15:19:17 +0700

    update pagination

--- www/admin/viewAllAcc/viewAllAcc.php
+++ www/admin/viewAllAcc/viewAllAcc.php
@@ -11,20 +11,28 @@ usort($accounts,'created_time_cmp');
 
 $_SESSION['accounts'] = $accounts;
 
+$numberOfPages = ceil(count($_SESSION['accounts'])) / $numberOfItemsPerPage;
+
 if ($accounts) {
-    foreach ($accounts as $acc) {
+    $totalLoopPerPage = 0;
+    if (count($accounts) > $page*$numberOfItemsPerPage){
+        $totalLoopPerPage = $page*$numberOfItemsPerPage;
+      } else {
+        $totalLoopPerPage = count($accounts);
+      }
+
+    for ($i = $thisPageFirstResult; $i<$totalLoopPerPage; $i++) {
         echo '<tr>';
+        
+        echo '<td>' .'<a href="display-account.php?email='.$accounts[$i][0].'"'.'>'.$accounts[$i][0].'</a></td>';
+        
+        echo '<td>' . $accounts[$i][1] .'</td>';
     
-        echo '<td><a href="display-account.php?email='.$acc[0].'"'.'>'.$acc[0].'</a></td>';
+        echo '<td>' . $accounts[$i][2] .'</td>';
     
-        echo '<td>' . $acc[1] .'</td>';
+        echo '<td>' . $accounts[$i][3] .'</td>';
     
-        echo '<td>' . $acc[2] .'</td>';
-    
-        echo '<td>' . $acc[3] .'</td>';
-    
-        echo '<td>' . $acc[4] .'</td>';
-    
+        echo '<td>' . $accounts[$i][4] .'</td>';
     
         echo '</tr>';
     }
