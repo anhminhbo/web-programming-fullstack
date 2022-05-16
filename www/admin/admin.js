790e497a42a5afc9667f08dc94dbb70a7df8ad16
Minh Nguyens <minhnguyen@wee.vn>
2022-05-16 17:11:07 +0700

    update pagination js

--- www/admin/admin.js
+++ www/admin/admin.js
@@ -5,3 +5,22 @@ searchBtn.addEventListener("click", (e) => {
 
   location.href = "admin-index.php?page=1&search=" + searchInput.value;
 });
+
+const anchorPageLinks = document.querySelectorAll(".page-link");
+
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const page = urlParams.get("page");
+
+for (const pageLink of anchorPageLinks) {
+  const queryFromAnchor = pageLink.href.split("?")[1];
+  const urlParam = new URLSearchParams(queryFromAnchor);
+  const pageQuery = urlParam.get("page");
+  if (page == pageQuery) {
+    pageLink.classList.add("focus");
+  } else {
+    pageLink.classList.remove("focus");
+  }
+}
