56aaaa2a29638a6ca52a7ce05b5b7edc377aecf0
KyAnh <s3914486@rmit.edu.vn>
2022-05-16 11:33:29 +0700

    Update Display-account

--- www/admin/display-account.php
+++ www/admin/display-account.php
@@ -1,4 +1,4 @@
-<?php
+<!-- 
 
 // https://www.youtube.com/watch?v=KdgqrHWg69M
 
@@ -12,4 +12,40 @@
 
 // Display created at
 
-?>
+ -->
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Account</title>
+    <link rel="stylesheet" href="admin-index.css">
+</head>
+<body>
+    <div class="display-account-container">
+        <div class="account account-email">
+            <h2>User-Email</h2>
+            <p>???????</p>
+        </div>
+        <div class="account account-password">
+            <h2>User-Password</h2>
+            <div class="container-password">
+                <input type="Password">
+                <button class="btn-rest">Rest</button>
+            </div>
+        </div>
+        <div class="account account-fname">
+            <h2>User-fname</h2>
+            <p>???????</p>
+        </div>
+        <div class="account account-lname">
+            <h2>User-lname</h2>
+            <p>????????</p>
+        </div>
+        <div class="account create_at">
+            <h2>Create_at</h2>
+        </div>
+    </div>
+</body>
+</html>
